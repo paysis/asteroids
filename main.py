2636529dc1212d3873acb12da0b571c092169b1e
paysis <paysis21@gmail.com>
2024-10-28 17:40:32 +0300

    Adds player movement, sprite groups

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import pygame
 import constants
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # groups
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
+    # game objects
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # update
+        for u in updatables:
+            u.update(dt)
+
+        # render
         screen.fill((0,0,0))
+
+        for d in drawables:
+            d.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
